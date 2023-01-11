392715ca2ae793f042af278bf185cf539a2d9b53
OlhaRyzha <104225380+OlhaRyzha@users.noreply.github.com>
2023-01-11 23:57:10 +0000

    Deploying to gh-pages from @ OlhaRyzha/goit-js-hw-09@1594ecc5842698454712735f32e3fc050b1782d9 🚀

---
+++ 01-color-switcher.f4fd5ebe.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body,o=0;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.f4fd5ebe.js.map
